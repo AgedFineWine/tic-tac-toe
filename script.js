ec6e7a7d6bcf0fac450872d3ba7333f63eb8f698
Evan He <eh4359@rit.edu>
2024-08-21 15:26:48 -0400

    swapped to ES6 classes syntax

--- script.js
+++ script.js
@@ -13,40 +13,42 @@ class Player {
         this.id.add(id);
     }
 }
-    
-function Gameboard() {
-    // null gets replaced with a Player object later on
-    this.gameboard = [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
-    ];
 
-    // all possible cell combinations a player must occupy before winning
-    this.win = [
-        new Set([1, 2, 3]), new Set([4, 5, 6]), new Set([7, 8, 9]),
-        new Set([1, 4, 7]), new Set([2, 5, 8]), new Set([3, 6, 9]),
-        new Set([1, 5, 9]), new Set([3, 5, 7])
-    ];
+class Gameboard {
+    constructor() {
+        // null gets replaced with a Player object later on
+        this.gameboard = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
+        // all possible cell combinations a player must occupy before winning
+        this.win = [
+            new Set([1, 2, 3]), new Set([4, 5, 6]), new Set([7, 8, 9]),
+            new Set([1, 4, 7]), new Set([2, 5, 8]), new Set([3, 6, 9]),
+            new Set([1, 5, 9]), new Set([3, 5, 7])
+        ];
 
-    // used to change the this.gameboard in the changeCell() method
-    this.idMap = new Map([
-        [1, { row: 0, col: 0 }],
-        [2, { row: 0, col: 1 }],
-        [3, { row: 0, col: 2 }],
-        [4, { row: 1, col: 0 }],
-        [5, { row: 1, col: 1 }],
-        [6, { row: 1, col: 2 }],
-        [7, { row: 2, col: 0 }],
-        [8, { row: 2, col: 1 }],
-        [9, { row: 2, col: 2 }],
-    ]);
+        // used to change the this.gameboard in the changeCell() method
+        this.idMap = new Map([
+            [1, { row: 0, col: 0 }],
+            [2, { row: 0, col: 1 }],
+            [3, { row: 0, col: 2 }],
+            [4, { row: 1, col: 0 }],
+            [5, { row: 1, col: 1 }],
+            [6, { row: 1, col: 2 }],
+            [7, { row: 2, col: 0 }],
+            [8, { row: 2, col: 1 }],
+            [9, { row: 2, col: 2 }],
+        ]);
 
-    this.getBoard = function() {
+    }
+
+    getBoard() {
         return this.gameboard;
     }
 
-    this.changeCell = function(player, id) {
+    changeCell(player, id) {
         const { row, col } = this.idMap.get(id);
 
         if (this.gameboard[row][col] == null) {
@@ -55,7 +57,7 @@ function Gameboard() {
     }
 
     // iterate through all possible win possibilties
-    this.checkWin = function(playerObj) {
+    checkWin(playerObj) {
         // need at least one case is true hence we use the .some() method
         return this.win.some(winSet => {
             // player must occupy all of the cells per win possibility
@@ -65,7 +67,7 @@ function Gameboard() {
         });
     }
 
-    this.checkDraw = function(playerObj) {
+    checkDraw(playerObj) {
         const board = this.getBoard();
         // the first condition isn't really necessary because of how
         // implemented the code body in addEventListener
@@ -75,25 +77,24 @@ function Gameboard() {
     }
 }
 
-function Game() {
-    // Separate initialization logic
-    this.init = function() {
+class Game {
+    init() {
         this.gameboard = new Gameboard();
         this.playerX = new Player('X');
         this.playerO = new Player('O');
         this.currentPlayer = this.playerX;
         this.setResetBtn();
         this.buttonSetUp();
-    };
+    }
 
-    this.buttonSetUp = function() {
+    buttonSetUp() {
         for (let id = 1; id <= 9; id++) {
             const btn = document.getElementById(`${id}`);
             btn.addEventListener('click', event => this.handleBtnClick(id, btn));
         }
-    };
+    }
 
-    this.handleBtnClick = function(id, btn) {
+    handleBtnClick(id, btn) {
         // if the text is empty it is truthy
         if (!btn.textContent) {
             // update cells and player idSet for occupied cells
@@ -113,7 +114,7 @@ function Game() {
             } else if (this.gameboard.checkDraw(this.currentPlayer)) {
                 alert("It's a tie!");
                 player.textContent = '';
-                msg.textContent = "It's a tie!"; 
+                msg.textContent = "It's a tie!";
             } else {
                 // if the current player name is X, swaps to playerO if not it goes to playerX
                 this.currentPlayer = this.currentPlayer.name === this.playerX.name ? this.playerO : this.playerX;
@@ -124,14 +125,14 @@ function Game() {
                 // change the color of the span
             }
         }
-    };
+    }
 
-    this.setResetBtn = function() {
+    setResetBtn() {
         const resetBtn = document.querySelector('#reset');
         resetBtn.addEventListener('click', () => this.resetGame());
     }
 
-    this.resetGame = function() {
+    resetGame() {
         // make new buttons to replace the old ones
         document.querySelectorAll('.cell').forEach(btn => {
             btn.textContent = '';
@@ -147,12 +148,10 @@ function Game() {
         player.textContent = "Player X's";
         player.style.color = 'red';
         msg.textContent = 'Move';
-
-    };
-
+    }
 }
 
-const game = (() => {
+(() => {
     const newGame = new Game();
     // Initialize the game on creation
     newGame.init();
