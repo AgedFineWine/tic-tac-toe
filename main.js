2c615eeeb2dceab67ef5af7b56235ce9d573bcac
Evan He <eh4359@rit.edu>
2024-08-21 20:18:33 -0400

    renamed script.js to main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+import Game from "./logic/game.js";
+
+(() => {
+    const newGame = new Game();
+    // Initialize the game on creation
+    newGame.init();
+})();
--- script.js
+++
@@ -1,7 +0,0 @@
-import Game from "./logic/game.js";
-
-(() => {
-    const newGame = new Game();
-    // Initialize the game on creation
-    newGame.init();
-})();
