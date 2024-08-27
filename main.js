541aa914dcfbfc8af30f2a2d2a80c489469363d6
Evan He <eh4359@rit.edu>
2024-08-26 22:45:35 -0400

    fixed night mode button position and button background in dark mode

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 import Game from "./logic/game.js";
 
 function toggleNightMode(event) {
-    const btn = event.target;
+    const btn = event.currentTarget;
 
     if (document.body.classList.contains('dark-mode')) {
         document.body.classList.remove('dark-mode');
         btn.classList.remove('dark-mode');
     } else {
+        console.log(btn)
         document.body.classList.add('dark-mode');
         btn.classList.add('dark-mode');
     }
